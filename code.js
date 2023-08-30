fcbcb8b5c6d08dedc262dd479e4861c2a280c2b2
Daniel Leaubon <leaubondaniel@gmail.com>
2023-08-30 12:39:41 -0300

    Etapa 2

---
+++ code.js
@@ -0,0 +1,32 @@
+function pessoa (){
+    const form = document.querySelector('#form');
+    const resultado = document.querySelector('#resultado');
+
+    const pessoas = [];
+    
+    function recebeEventoForm(evento){
+    evento.preventDefault();
+    const nome = form.querySelector('#Nome');
+    const sobrenome = form.querySelector('#Sobrenome');
+    const peso = form.querySelector('#Peso');
+    const altura = form.querySelector('#Altura');
+    
+        let pessoas = {
+            nome: nome.value,
+            sobrenome: sobrenome.value,
+            peso: peso.value,
+            alura: altura.value 
+        };
+        console.log(pessoas);
+        if (pessoas.nome == '' || pessoas.sobrenome == ''||pessoas.peso == '' || pessoas.altura == ''){
+            resultado.innerHTML += `<p> Algum dado não confere</p> `
+        } else {
+            resultado.innerHTML += `<p>${nome.value} ${sobrenome.value}`+
+            ` ${peso.value}KG e ${altura.value}cm</p>`;
+        }
+        
+    }
+    form.addEventListener('submit',  recebeEventoForm);
+
+}
+pessoa()
